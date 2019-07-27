0fb9512cd69153aac562b6f03cf64cc3254390f6
Alejandro <alexrodriguezserrano1595@gmail.com>
2019-07-27 16:04:16 -0500

    add textbox operations

--- operator-morena/operator-morena/Form2.Designer.cs
+++ operator-morena/operator-morena/Form2.Designer.cs
@@ -49,6 +49,13 @@
             this.tbPopulation = new System.Windows.Forms.TextBox();
             this.tbSection = new System.Windows.Forms.TextBox();
             this.tbMunicipality = new System.Windows.Forms.TextBox();
+            this.materialLabel6 = new MaterialSkin.Controls.MaterialLabel();
+            this.materialLabel7 = new MaterialSkin.Controls.MaterialLabel();
+            this.materialLabel8 = new MaterialSkin.Controls.MaterialLabel();
+            this.materialLabel9 = new MaterialSkin.Controls.MaterialLabel();
+            this.tbEmail = new System.Windows.Forms.TextBox();
+            this.tbPhone = new System.Windows.Forms.TextBox();
+            this.rtbComents = new System.Windows.Forms.RichTextBox();
             this.toolStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
@@ -155,7 +162,7 @@
             // 
             this.pictureBox1.Location = new System.Drawing.Point(522, 114);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(119, 95);
+            this.pictureBox1.Size = new System.Drawing.Size(144, 118);
             this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
             // 
@@ -180,7 +187,7 @@
             this.materialLabel2.Depth = 0;
             this.materialLabel2.Font = new System.Drawing.Font("Roboto", 11F);
             this.materialLabel2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.materialLabel2.Location = new System.Drawing.Point(672, 162);
+            this.materialLabel2.Location = new System.Drawing.Point(672, 145);
             this.materialLabel2.MouseState = MaterialSkin.MouseState.HOVER;
             this.materialLabel2.Name = "materialLabel2";
             this.materialLabel2.Size = new System.Drawing.Size(57, 19);
@@ -194,7 +201,7 @@
             this.materialLabel3.Depth = 0;
             this.materialLabel3.Font = new System.Drawing.Font("Roboto", 11F);
             this.materialLabel3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.materialLabel3.Location = new System.Drawing.Point(518, 232);
+            this.materialLabel3.Location = new System.Drawing.Point(534, 430);
             this.materialLabel3.MouseState = MaterialSkin.MouseState.HOVER;
             this.materialLabel3.Name = "materialLabel3";
             this.materialLabel3.Size = new System.Drawing.Size(67, 19);
@@ -208,7 +215,7 @@
             this.materialLabel4.Depth = 0;
             this.materialLabel4.Font = new System.Drawing.Font("Roboto", 11F);
             this.materialLabel4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.materialLabel4.Location = new System.Drawing.Point(518, 276);
+            this.materialLabel4.Location = new System.Drawing.Point(534, 474);
             this.materialLabel4.MouseState = MaterialSkin.MouseState.HOVER;
             this.materialLabel4.Name = "materialLabel4";
             this.materialLabel4.Size = new System.Drawing.Size(79, 19);
@@ -222,7 +229,7 @@
             this.materialLabel5.Depth = 0;
             this.materialLabel5.Font = new System.Drawing.Font("Roboto", 11F);
             this.materialLabel5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.materialLabel5.Location = new System.Drawing.Point(781, 276);
+            this.materialLabel5.Location = new System.Drawing.Point(797, 474);
             this.materialLabel5.MouseState = MaterialSkin.MouseState.HOVER;
             this.materialLabel5.Name = "materialLabel5";
             this.materialLabel5.Size = new System.Drawing.Size(80, 19);
@@ -238,37 +245,124 @@
             // 
             // tbAlias
             // 
-            this.tbAlias.Location = new System.Drawing.Point(763, 162);
+            this.tbAlias.Location = new System.Drawing.Point(763, 146);
             this.tbAlias.Name = "tbAlias";
             this.tbAlias.Size = new System.Drawing.Size(252, 20);
             this.tbAlias.TabIndex = 12;
             // 
             // tbPopulation
             // 
-            this.tbPopulation.Location = new System.Drawing.Point(863, 275);
+            this.tbPopulation.Location = new System.Drawing.Point(879, 473);
             this.tbPopulation.Name = "tbPopulation";
             this.tbPopulation.Size = new System.Drawing.Size(100, 20);
             this.tbPopulation.TabIndex = 13;
             // 
             // tbSection
             // 
-            this.tbSection.Location = new System.Drawing.Point(606, 231);
+            this.tbSection.Location = new System.Drawing.Point(622, 429);
             this.tbSection.Name = "tbSection";
             this.tbSection.Size = new System.Drawing.Size(95, 20);
             this.tbSection.TabIndex = 14;
             // 
             // tbMunicipality
             // 
-            this.tbMunicipality.Location = new System.Drawing.Point(606, 275);
+            this.tbMunicipality.Location = new System.Drawing.Point(622, 473);
             this.tbMunicipality.Name = "tbMunicipality";
             this.tbMunicipality.Size = new System.Drawing.Size(160, 20);
             this.tbMunicipality.TabIndex = 15;
+            // 
+            // materialLabel6
+            // 
+            this.materialLabel6.AutoSize = true;
+            this.materialLabel6.BackColor = System.Drawing.Color.Transparent;
+            this.materialLabel6.Depth = 0;
+            this.materialLabel6.Font = new System.Drawing.Font("Roboto", 11F);
+            this.materialLabel6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialLabel6.Location = new System.Drawing.Point(672, 177);
+            this.materialLabel6.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialLabel6.Name = "materialLabel6";
+            this.materialLabel6.Size = new System.Drawing.Size(59, 19);
+            this.materialLabel6.TabIndex = 16;
+            this.materialLabel6.Text = "Correo:";
+            // 
+            // materialLabel7
+            // 
+            this.materialLabel7.AutoSize = true;
+            this.materialLabel7.BackColor = System.Drawing.Color.Transparent;
+            this.materialLabel7.Depth = 0;
+            this.materialLabel7.Font = new System.Drawing.Font("Roboto", 11F);
+            this.materialLabel7.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialLabel7.Location = new System.Drawing.Point(672, 213);
+            this.materialLabel7.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialLabel7.Name = "materialLabel7";
+            this.materialLabel7.Size = new System.Drawing.Size(73, 19);
+            this.materialLabel7.TabIndex = 17;
+            this.materialLabel7.Text = "Teléfono:";
+            // 
+            // materialLabel8
+            // 
+            this.materialLabel8.AutoSize = true;
+            this.materialLabel8.BackColor = System.Drawing.Color.Transparent;
+            this.materialLabel8.Depth = 0;
+            this.materialLabel8.Font = new System.Drawing.Font("Roboto", 11F);
+            this.materialLabel8.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialLabel8.Location = new System.Drawing.Point(518, 251);
+            this.materialLabel8.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialLabel8.Name = "materialLabel8";
+            this.materialLabel8.Size = new System.Drawing.Size(93, 19);
+            this.materialLabel8.TabIndex = 18;
+            this.materialLabel8.Text = "Calificación:";
+            // 
+            // materialLabel9
+            // 
+            this.materialLabel9.AutoSize = true;
+            this.materialLabel9.BackColor = System.Drawing.Color.Transparent;
+            this.materialLabel9.Depth = 0;
+            this.materialLabel9.Font = new System.Drawing.Font("Roboto", 11F);
+            this.materialLabel9.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialLabel9.Location = new System.Drawing.Point(518, 294);
+            this.materialLabel9.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialLabel9.Name = "materialLabel9";
+            this.materialLabel9.Size = new System.Drawing.Size(112, 19);
+            this.materialLabel9.TabIndex = 19;
+            this.materialLabel9.Text = "Observaciones:";
+            // 
+            // tbEmail
+            // 
+            this.tbEmail.Location = new System.Drawing.Point(763, 178);
+            this.tbEmail.Name = "tbEmail";
+            this.tbEmail.Size = new System.Drawing.Size(252, 20);
+            this.tbEmail.TabIndex = 20;
+            this.tbEmail.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
+            // 
+            // tbPhone
+            // 
+            this.tbPhone.Location = new System.Drawing.Point(763, 212);
+            this.tbPhone.Name = "tbPhone";
+            this.tbPhone.Size = new System.Drawing.Size(252, 20);
+            this.tbPhone.TabIndex = 21;
+            this.tbPhone.TextChanged += new System.EventHandler(this.TextBox2_TextChanged);
+            // 
+            // rtbComents
+            // 
+            this.rtbComents.Location = new System.Drawing.Point(522, 317);
+            this.rtbComents.Name = "rtbComents";
+            this.rtbComents.Size = new System.Drawing.Size(578, 72);
+            this.rtbComents.TabIndex = 22;
+            this.rtbComents.Text = "";
             // 
             // wfDashBoard
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1130, 687);
+            this.Controls.Add(this.rtbComents);
+            this.Controls.Add(this.tbPhone);
+            this.Controls.Add(this.tbEmail);
+            this.Controls.Add(this.materialLabel9);
+            this.Controls.Add(this.materialLabel8);
+            this.Controls.Add(this.materialLabel7);
+            this.Controls.Add(this.materialLabel6);
             this.Controls.Add(this.tbMunicipality);
             this.Controls.Add(this.tbSection);
             this.Controls.Add(this.tbPopulation);
@@ -316,5 +410,12 @@
         private System.Windows.Forms.TextBox tbPopulation;
         private System.Windows.Forms.TextBox tbSection;
         private System.Windows.Forms.TextBox tbMunicipality;
+        private MaterialSkin.Controls.MaterialLabel materialLabel6;
+        private MaterialSkin.Controls.MaterialLabel materialLabel7;
+        private MaterialSkin.Controls.MaterialLabel materialLabel8;
+        private MaterialSkin.Controls.MaterialLabel materialLabel9;
+        private System.Windows.Forms.TextBox tbEmail;
+        private System.Windows.Forms.TextBox tbPhone;
+        private System.Windows.Forms.RichTextBox rtbComents;
     }
 }
--- operator-morena/operator-morena/Form2.cs
+++ operator-morena/operator-morena/Form2.cs
@@ -37,5 +37,15 @@ namespace operator_morena
             Application.ExitThread();
             Application.Exit();
         }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
